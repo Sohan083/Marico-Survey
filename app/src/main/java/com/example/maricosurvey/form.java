36caec40686e404bea3eb3701382a5fcd7cc6c5c
Sohan083 <31314000+Sohan083@users.noreply.github.com>
2020-03-17 22:29:26 +0600

    1st sectiond done

--- app/src/main/java/com/example/maricosurvey/form.java
+++ app/src/main/java/com/example/maricosurvey/form.java
@@ -1,13 +1,26 @@
 package com.example.maricosurvey;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class form extends AppCompatActivity {
+
+    String[] sDropList = new String[] {"Male", "Female"};
+    String[] bcsDropList = new String[] {"Yes", "No"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        Spinner sdropdown = findViewById(R.id.sdropdown);
+        Spinner bcsdropdown = findViewById(R.id.bcsdropdown);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sDropList);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, bcsDropList);
+        sdropdown.setAdapter(adapter1);
+        bcsdropdown.setAdapter(adapter2);
     }
+
 }
